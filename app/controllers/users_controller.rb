80fb138f8f059f67aa0f407b7949b6b4a037c413
Yoshinori Iwasaki <wassyoi0531@gmail.com>
2016-12-19 01:36:33 +0900

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,13 +11,13 @@ class UsersController < ApplicationController
   def create 
   	@user = User.new(user_params)
   	if @user.save 
+      log_in @user
   		flash[:success] = "Welcome to the sample App!"
   		redirect_to @user  #redirect_to user_url(@user)と同値
   	else
   		render 'new'
   	end
   end
-
 
   private 
 
